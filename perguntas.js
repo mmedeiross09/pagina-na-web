c196336a23f39f5adc5f2894c2f6b9c7e4247c56
mmedeiross09 <medeiros.siqueira@escola.pr.gov.br>
2025-06-04 10:40:30 -0300

    04/06

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Artes',
+    'Qual é a importância da originalidade na pintura? ',
+    'A originalidade é essencial na pintura, pois é através dela que o artista consegue transmitir sua visão única do mundo. Cada pintor possui um estilo próprio e suas obras são como uma extensão de sua personalidade. '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Astronomia',
+    ' Por que a Lua não tem atmosfera?
+    'A gravidade lunar, um sexto da da Terra, não consegue reter os gases que formam uma atmosfera. As moléculas dos gases que formam a atmosfera da Terra estão em constante movimento, mas para escapar para o espaço precisam ultrapassar a velocidade de 11 quilômetros por segundo. Só gases muito leves, como o hidrogênio, se movem tão rápido. Para fugir à gravidade da Lua, basta a velocidade de 2 quilômetros por segundo.
+ '
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    ' Artes',
+    ' Como a técnica da pintura pode influenciar as emoções transmitidas?',
+    'A técnica utilizada pelo pintor também pode influenciar as emoções transmitidas pela obra de arte. Pinceladas rápidas e soltas podem transmitir energia e movimento, enquanto pinceladas suaves e delicadas podem transmitir tranquilidade e delicadeza. '
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Astronomia',
+    ' O que aconteceria se a Lua desaparecesse?
+    'A gravidade da Terra e a da Lua se influenciam mutuamente. O sumiço repentino da Lua tornaria o movimento de rotação da Terra caótico como o de um pião em baixa velocidade. Seria catastrófico para a vida no planeta, com alterações drásticas do clima. Períodos quentíssimos se alternariam, de forma aleatória, com fases de frio glacial. Os animais com mais chances de sobrevivência seriam os aquáticos, já que a temperatura da água varia mais lentamente. Embora um afastamento súbito da Lua seja improvável, sabe-se que ela está se distanciando da Terra à razão de alguns centímetros por ano. Por enquanto, não há motivo para pânico: bilhões de anos nos separam de um afastamento da Lua capaz de provocar alterações em nosso planeta '
 )
